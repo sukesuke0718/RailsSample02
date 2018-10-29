fa01fc41dcff9f4f6d1953de0c73b842883658a9
ユウ <sukesuke0718@gmail.com>
2018-10-29 23:33:08 +0900

    フォーム追加

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -1,6 +1,38 @@
+require 'date'
+
 class BookController < ApplicationController
+
   def info
+    # 現在の時刻を取得する
+    @time = Date.today
   end
+
   def list
+    @name = params[:name]
+    @text = ""
+    random = Random.new
+
+    if @name == "" then
+
+      @text = "名前を入れてね"
+
+    else
+
+      case  random.rand(1..3)
+        when 1
+          @text = "お帰りなさいませ\n" + @name + "様♡"
+        when 2
+          if @name.length >= 2
+            @name = @name[0,2]
+          end
+          @text = @name + "にゃん、\n" + "おかえりにゃんにゃん♪"
+        when 3
+          @text = "c"
+        else
+          @text = "残念ハズレ"
+      end
+
+    end
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'user/info'
   get '/' => 'book#info'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/list' => 'book#list'
+  # フォームのデータを渡す
+  post '/list' => 'book#list'
 end
